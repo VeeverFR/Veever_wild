0828fb41ac1b440013554a34f67460acaed4713f
Sarah Smandack <sarah.smandack@gmail.com>
2024-12-18 16:48:47 +0100

    create CSS variables and shortcut for @src

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,8 @@
 import "./App.css";
-import krokmou from "@src/assets/images/krokmou.jpg";
 
 function App() {
   return (
     <>
-      <img src={krokmou} alt="" />
       <h1>pouet</h1>
     </>
   );
