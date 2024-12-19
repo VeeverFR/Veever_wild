2080c700a5b3556f81900ca136431e8e591ce208
nicochnz <chiche.nicolas@gmail.com>
2024-12-19 15:51:46 +0100

    feat preheader & discovery section

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,9 @@
 import "./App.css";
-import Discovery from "./components/Discovery/Discovery";
-import PreHeader from "./components/PreHeader/PreHeader";
 import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
     <>
-      <PreHeader />
-      <Discovery />
       <HomePage />
     </>
   );
--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,12 @@
+import Discovery from "../../components/Discovery/Discovery";
 import NewsLetter from "../../components/NewsLetter/NewsLetter";
+import PreHeader from "../../components/PreHeader/PreHeader";
 
 export default function HomePage() {
   return (
     <>
+      <PreHeader />
+      <Discovery />
       <NewsLetter />
     </>
   );
