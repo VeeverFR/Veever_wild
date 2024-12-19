5398b234eb663ba510b8035a28b52155a2908571
nicochnz <chiche.nicolas@gmail.com>
2024-12-19 15:47:19 +0100

    feat preheader & discovery section

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import Discovery from "./components/Discovery/Discovery";
+import PreHeader from "./components/PreHeader/PreHeader";
 import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
     <>
+      <PreHeader />
+      <Discovery />
       <HomePage />
     </>
   );
