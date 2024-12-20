4123a6224400bb84695d82b25d424a3ae9a9e25c
Nino Jautee <jauteenino@gmail.com>
2024-12-20 14:15:00 +0100

    Revert "create CSS variables and shortcut for @src"
    This reverts commit 0828fb41ac1b440013554a34f67460acaed4713f.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import krokmou from "@src/assets/images/krokmou.jpg";
 
 function App() {
   return (
     <>
+      <img src={krokmou} alt="" />
       <h1>pouet</h1>
     </>
   );
