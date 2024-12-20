8efc00b0d44f2b78a9e3977d64a974a2b36a1e0a
Raphaël Streiff <raphaelstreiff93@gmail.com>
2024-12-20 10:35:08 +0100

    Modifications apportées à itineraryCard.tsx~

--- client/src/components/ItinarySection/itineraryCard/itineraryCard.tsx
+++ client/src/components/ItinarySection/itineraryCard/itineraryCard.tsx
@@ -1,20 +1,19 @@
-
 interface CardProps {
-    title: string;
-    imageSrc: string;
-    onReserve: () => void;
+  title: string;
+  imageSrc: string;
+  onReserve: () => void;
 }
 
 const Card = ({ title, imageSrc, onReserve }: CardProps) => {
-    return (
-        <article className="card">
-            <img src={imageSrc} alt={title} className="card__image" />
-            <h3 className="card-title">{title}</h3>
-            <button className="reserve-button" onClick={onReserve}>
-                Réserver
-            </button>
-        </article>
-    );
+  return (
+    <article className="card">
+      <img src={imageSrc} alt={title} className="card__image" />
+      <h3 className="card-title">{title}</h3>
+      <button type="button" className="reserve-button" onClick={onReserve}>
+        Réserver
+      </button>
+    </article>
+  );
 };
 
 export default Card;
