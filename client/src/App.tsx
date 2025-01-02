58ea7ee7738052d3053e46d8a7556608a64509ec
Raphaël Streiff <raphaelstreiff93@gmail.com>
2025-01-02 14:21:19 +0100

    add component itinerarySection and create modules clientsActions and also clientsRepository

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,10 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import "./App.css";
-import ItinerarySection from "./components/itinerarySection/itinerarySection.tsx";
 
 function App() {
   return (
     <>
-      <h1>pouet</h1>
-      <ItinerarySection />
       <NavBar />
       <Outlet />
     </>
